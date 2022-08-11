37d8236ec43bc88cc7fa645edc622217d16d8551
sinhashlok <shlokjp@gmail.com>
2022-08-12 00:31:40 +0530

    update

---
+++ Recursion/Medium/Subsets_78.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/subsets/
+
+package DSApractice.Recursion.Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets_78 {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> outer = new ArrayList<>();
+
+        outer.add(new ArrayList<>());
+
+        for (int num : nums) {
+            int n = outer.size();
+
+            for (int i = 0; i < n; i++) {
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+
+        return outer;
+    }
+}
