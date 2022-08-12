b8464e4c6b84cca04bd05bd61487878c5d7a2df9
sinhashlok <shlokjp@gmail.com>
2022-08-12 10:46:48 +0530

    update

---
+++ Strings/Easy/Valid_Anagram_242.java
@@ -0,0 +1,23 @@
+package DSApractice.Strings.Easy;
+
+public class Valid_Anagram_242 {
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] freqs = new int[26];
+        for (char c: s.toCharArray()) {
+            freqs[c -'a']++;
+        }
+
+        for (char c: t.toCharArray()) {
+            if (--freqs[c -'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
