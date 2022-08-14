7b7ee2d960584d02f90350f9fc4f8223a374e7c6
sinhashlok <shlokjp@gmail.com>
2022-08-14 22:55:41 +0530

    update

---
+++ Strings/Easy/Reverse_String_344.java
@@ -0,0 +1,18 @@
+package DSApractice.Strings.Easy;
+
+public class Reverse_String_344 {
+
+    public void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length - 1;
+
+        while (i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+
+            i++;
+            j--;
+        }
+    }
+}
