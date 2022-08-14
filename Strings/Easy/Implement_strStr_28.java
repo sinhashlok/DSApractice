72e11a4acf73d5af48513e48a67a846d05632c3d
sinhashlok <shlokjp@gmail.com>
2022-08-14 22:50:19 +0530

    update

---
+++ Strings/Easy/Implement_strStr_28.java
@@ -0,0 +1,8 @@
+package DSApractice.Strings.Easy;
+
+public class Implement_strStr_28 {
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
