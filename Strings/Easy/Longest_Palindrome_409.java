c973360f54d9ba1f2a4ea900028d34897e153052
sinhashlok <shlokjp@gmail.com>
2022-08-19 12:04:22 +0530

    update

---
+++ Strings/Easy/Longest_Palindrome_409.java
@@ -0,0 +1,26 @@
+package DSApractice.Strings.Easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Longest_Palindrome_409 {
+
+    public int longestPalindrome(String s) {
+        if (s == null) {
+            return 0;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch,0)+1);
+        }
+
+        int evenCount = 0;
+        for(int count : map.values()){
+            evenCount += (count % 2 == 0) ? count : count-1;
+        }
+
+        return evenCount == s.length() ? evenCount : evenCount + 1;
+    }
+}
