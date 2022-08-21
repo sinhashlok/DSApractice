0a8fdd370ad70e1e354805feb7126f743d281c6d
sinhashlok <shlokjp@gmail.com>
2022-08-21 23:31:19 +0530

    update

--- Stack/Medium/Online_Stock_Span_901.java
+++ Stack/Medium/Online_Stock_Span_901.java
@@ -8,19 +8,23 @@ import java.util.Stack;
 
 public class Online_Stock_Span_901 {
 
-    // we store
+    // we store the price as well the number of stock price less than it in a stack of array
+    // stack (arr[price, res])
+    // res - number of stocks less the price
     Stack<int[]> stack;
-    public StockSpanner() {
+    public void StockSpanner() {
         stack = new Stack<>();
     }
 
     public int next(int price) {
         int res = 1;
 
+        // while previous larger price is lower than current new stock price, keep removing it from array
         while (!stack.isEmpty() && stack.peek()[0] <= price) {
             res += stack.pop()[1];
         }
 
+        // now add the current new price and the number of stocks less that it consecutively (minimum 1) than price in stack
         stack.push(new int[]{price, res});
         return res;
     }
