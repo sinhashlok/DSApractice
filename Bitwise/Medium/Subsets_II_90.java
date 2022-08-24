642f43bf13a4b9a63d336ba3c96049256809e2d3
sinhashlok <shlokjp@gmail.com>
2022-08-24 19:36:35 +0530

    update

--- Bitwise/Medium/Subsets_II_90.java
+++ Bitwise/Medium/Subsets_II_90.java
@@ -6,17 +6,21 @@ import java.util.List;
 
 public class Subsets_II_90 {
 
-    public List<List<Integer>> subsetsWithDup(int[] arr) {
-        Arrays.sort(arr);
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+
         List<List<Integer>> outer = new ArrayList<>();
-        outer.add(new ArrayList<>());
+        outer.add(new ArrayList<>());   // add empty subset
 
         int start = 0, end = 0;
-        for (int i = 0; i < arr.length; i++) {
+        // start and help in preventing duplicated subsets
+
+        for (int i = 0; i < nums.length; i++) {
             start = 0;
 
             // if current and previous element is same, s = e + 1
-            if (i > 0 && arr[i] == arr[i - 1]) {
+            // this allows use to skip copying the subsets, due to which duplicated subsets can be generated
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 start = end + 1;
             }
 
@@ -25,7 +29,7 @@ public class Subsets_II_90 {
 
             for (int j = start; j < n; j++) {
                 List<Integer> internal = new ArrayList<>(outer.get(j));
-                internal.add(arr[i]);
+                internal.add(nums[i]);
                 outer.add(internal);
             }
         }
