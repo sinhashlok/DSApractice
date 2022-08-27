3708ee63c0266a6020ca49609f106dd76bcd9bf2
sinhashlok <shlokjp@gmail.com>
2022-08-27 20:16:01 +0530

    update

--- Searching/Medium/Search_in_Rotated_Sorted_Array_33.java
+++ Searching/Medium/Search_in_Rotated_Sorted_Array_33.java
@@ -4,64 +4,46 @@ package DSApractice.Searching.Medium;
 
 public class Search_in_Rotated_Sorted_Array_33 {
 
-    public int search(int[] nums, int target) {
-        int pivot = findPivot(nums);
+    public int searchLeet(int[] nums, int target) {
+        if(nums == null || nums.length == 0) {
+            return -1;
+        }
+        int l = 0, r = nums.length - 1, m = 0;
 
-        if (pivot == -1) {
-            return binarySearch(nums, target, 0 , nums.length - 1);
+        // find out the index of the smallest element.
+        while(l < r) {
+            m = l + (r - l) / 2;
+
+            if (nums[m] > nums[r]) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
         }
 
-        if (nums[pivot] == target) {
-            return pivot;
+        // since we now know the start, find out if the target is to left or right of start in the array.
+        int s = l;
+        l = 0; r = nums.length - 1;
+
+        if(target >= nums[s] && target <= nums[r]){
+            l = s;
+        } else {
+            r = s;
         }
 
-        if (target >= nums[0]) {
-            return binarySearch(nums, target, 0, pivot - 1);
-        }
+        // the regular search.
+        while(l <= r){
+            m = l + (r - l) / 2;
 
-        return binarySearch(nums, target, pivot + 1, nums.length - 1);
-    }
-
-    public int binarySearch(int[] arr, int target, int start, int end) {
-        while(start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (target < arr[mid]) {
-                end = mid - 1;
-            }
-            else if (target > arr[mid]) {
-                start = mid + 1;
-            }
-            else {
-                return mid;
+            if(nums[m] == target) {
+                return m;
+            } else if(nums[m] > target) {
+                r = m - 1;
+            } else {
+                l = m + 1;
             }
         }
-        return -1;
-    }
 
-    public int findPivot(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            // 4 cases over here
-            if (mid < end && arr[mid] > arr[mid + 1]) {
-                return mid;
-            }
-
-            if (mid > start && arr[mid] < arr[mid - 1]) {
-                return mid-1;
-            }
-
-            if (arr[mid] <= arr[start]) {
-                end = mid - 1;
-            }
-            else {
-                start = mid + 1;
-            }
-        }
         return -1;
     }
 }
