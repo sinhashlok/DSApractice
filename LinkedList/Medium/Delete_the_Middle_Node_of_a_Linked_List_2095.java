62f5ef914e68a703175a5a2b256aad5d53665d29
sinhashlok <shlokjp@gmail.com>
2022-08-29 17:56:47 +0530

    update

---
+++ LinkedList/Medium/Delete_the_Middle_Node_of_a_Linked_List_2095.java
@@ -0,0 +1,21 @@
+package DSApractice.LinkedList.Medium;
+
+public class Delete_the_Middle_Node_of_a_Linked_List_2095 {
+
+    public ListNode deleteMiddle(ListNode head) {
+        if (head.next == null) {
+            return null;
+        }
+
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        slow.val = slow.next.val;
+        slow.next = slow.next.next;
+
+        return head;
+    }
+}
