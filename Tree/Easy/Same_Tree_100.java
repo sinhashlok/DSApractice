848930d4d0a2084f845a6696777d247b80bedb34
sinhashlok <shlokjp@gmail.com>
2022-09-15 15:02:55 +0530

    update

---
+++ Tree/Easy/Same_Tree_100.java
@@ -0,0 +1,18 @@
+package DSApractice.Tree.Easy;
+
+public class Same_Tree_100 {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (q == null || p == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+    }
+}
