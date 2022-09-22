6e7706b92accca81d8940879f7bddea12cc0eb9c
sinhashlok <shlokjp@gmail.com>
2022-09-22 23:26:29 +0530

    update

--- Stack/Medium/Asteroid_Collision_735.java
+++ Stack/Medium/Asteroid_Collision_735.java
@@ -1,28 +1,33 @@
 package DSApractice.Stack.Medium;
 
+import java.util.Stack;
+
 public class Asteroid_Collision_735 {
 
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> stack = new Stack();
-        for (int ast: asteroids) {
-            collision: {
-                while (!stack.isEmpty() && ast < 0 && 0 < stack.peek()) {
-                    if (stack.peek() < -ast) {
-                        stack.pop();
-                        continue;
-                    } else if (stack.peek() == -ast) {
-                        stack.pop();
-                    }
-                    break collision;
+        Stack<Integer> s = new Stack<>();
+
+        for (int i : asteroids) {
+            if (i > 0) {    // i is positive
+                s.push(i);
+            } else {        // i is negative
+                while (!s.isEmpty() && s.peek() > 0 && s.peek() < Math.abs(i)) {
+                    s.pop();
                 }
-                stack.push(ast);
+
+                if (s.isEmpty() || s.peek() < 0) {  // add the negative element is stack becomes (or is) empty
+                    s.push(i);
+                } else if (i + s.peek() == 0) {     // equal
+                    s.pop();
+                }
             }
         }
 
-        int[] ans = new int[stack.size()];
-        for (int t = ans.length - 1; t >= 0; --t) {
-            ans[t] = stack.pop();
+        int[] arr = new int[s.size()];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            arr[i] = s.pop();
         }
-        return ans;
+
+        return arr;
     }
 }
