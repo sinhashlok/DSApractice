e77867ad21b9646e98f8eeae5d6fde63c8484664
sinhashlok <shlokjp@gmail.com>
2022-09-24 21:33:42 +0530

    update

--- Strings/Easy/Valid_Anagram_242.java
+++ Strings/Easy/Valid_Anagram_242.java
@@ -3,20 +3,26 @@ package DSApractice.Strings.Easy;
 public class Valid_Anagram_242 {
 
     public boolean isAnagram(String s, String t) {
+        // if length (no. of characters are not same => false
         if (s.length() != t.length()) {
             return false;
         }
 
+        // 26 alphabets
         int[] freqs = new int[26];
+        // increment index corresponding to each char of s
         for (char c: s.toCharArray()) {
             freqs[c -'a']++;
         }
 
+        // decrement index corresponding to each char of s
         for (char c: t.toCharArray()) {
+            // if c has more chara, one pos will go negative
             if (--freqs[c -'a'] < 0) {
                 return false;
             }
         }
+
 
         return true;
     }
