127894854aa89653b41f0fe5ca95e25ac708d90a
sinhashlok <shlokjp@gmail.com>
2022-10-04 18:36:47 +0530

    update

--- Arrays/Easy/Contains_Duplicate_II_219.java
+++ Arrays/Easy/Contains_Duplicate_II_219.java
@@ -6,15 +6,20 @@ import java.util.Set;
 public class Contains_Duplicate_II_219 {
 
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Set<Integer> set = new HashSet<>();
+        if (k == 0) {
+            return false;
+        }
 
-        for(int i = 0; i < nums.length; i++){
-            if(i > k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.size() == k + 1) {
                 set.remove(nums[i - k - 1]);
             }
-            if(!set.add(nums[i])) {
+            if (set.contains(nums[i])) {
                 return true;
             }
+
+            set.add(nums[i]);
         }
 
         return false;
