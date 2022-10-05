ded7a0b031c8e0944cc897e56722e027b25d39b4
sinhashlok <shlokjp@gmail.com>
2022-10-05 23:25:05 +0530

    update

--- Graph/Medium/All_Paths_From_Source_to_Target_797.java
+++ Graph/Medium/All_Paths_From_Source_to_Target_797.java
@@ -5,44 +5,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class All_Paths_From_Source_to_Target_797 {
-    public static void main(String[] args) {
-        int[][] g = {
-//                {4,3,1},
-//                {3,2,4},
-//                {3},
-//                {4},
-//                {}
-                {4,3,1},
-                {},
-                {1},
-                {2,1},
-                {3,1}
-        };
-
-        List<List<Integer>> l = allPathsSourceTarget(g);
-        System.out.println(l);
-        System.out.println(list);
-    }
-
-//    static void allPathsSourceTarget(int[][] graph) {
-//        for (int i = 0; i < graph[0].length; i++) {
-//            System.out.print(0 + " " + graph[0][i] + " ");
-//            print(graph, graph[0][i]);
-//        }
-//
-//    }
-//
-//    static void print(int[][] g, int pos) {
-//        if (g[pos].length == 0) {
-//            System.out.println();
-//            return;
-//        }
-//
-//        for (int i = 0; i < g[pos].length; i++) {
-//            System.out.print(g[pos][i] + " ");
-//            print(g, g[pos][i]);
-//        }
-//    }
 
     private List<List<Integer>> list;
     private List<Integer> temp;
