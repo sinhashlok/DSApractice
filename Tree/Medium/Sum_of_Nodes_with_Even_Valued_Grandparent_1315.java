f46667543c17c51b332ba14b4ce057fad315d8ac
sinhashlok <shlokjp@gmail.com>
2022-10-10 18:15:58 +0530

    update

--- Tree/Medium/Sum_of_Nodes_with_Even_Valued_Grandparent_1315.java
+++ Tree/Medium/Sum_of_Nodes_with_Even_Valued_Grandparent_1315.java
@@ -2,8 +2,9 @@ package DSApractice.Tree.Medium;
 
 public class Sum_of_Nodes_with_Even_Valued_Grandparent_1315 {
 
+    // My Logic
     private int count = 0;
-    public int sumEvenGrandparent(TreeNode root) {
+    public int sumEvenGrandparentMy(TreeNode root) {
         preOrder(root);
         return count;
     }
@@ -27,6 +28,27 @@ public class Sum_of_Nodes_with_Even_Valued_Grandparent_1315 {
         if (root.right != null) {
             count += (root.right.left != null ? root.right.left.val : 0);
             count += (root.right.right != null ? root.right.right.val : 0);
+        }
+    }
+
+
+
+    // Another approach - we keep track of current, parent and grandparent TreeNode
+    // regards - https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/discuss/477095/Easy-DFS-solution
+    private int sum = 0;
+    public int sumEvenGrandparent(TreeNode root) {
+        inorder(root, null, null);
+        return sum;
+    }
+
+    private void inorder(TreeNode current, TreeNode parent, TreeNode grandParent) {
+        if (current != null) {
+            if (grandParent != null && grandParent.val % 2 == 0) {
+                sum += current.val;
+            }
+
+            inorder(current.left, current, parent);// ( newChild, parent, GrandParent)
+            inorder(current.right, current, parent);
         }
     }
 }
