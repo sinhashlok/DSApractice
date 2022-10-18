a06b02beeab6943bcca5bde0047f464538a29a11
sinhashlok <shlokjp@gmail.com>
2022-10-18 14:54:18 +0530

    update

--- Strings/Easy/First_Unique_Character_in_a_String_387.java
+++ Strings/Easy/First_Unique_Character_in_a_String_387.java
@@ -18,22 +18,4 @@ public class First_Unique_Character_in_a_String_387 {
 
         return -1;
     }
-
-        // slightly faster
-    public int firstUniqChar(String s) {
-        int[] alp = new int[26];
-        char[] arr = s.toCharArray();   // make character array of string s
-
-        for(char c : arr ) {
-            alp[c - 'a']++;
-        }
-
-        for(int i = 0; i < arr.length; i++){
-            if (alp[arr[i] - 'a'] == 1) {
-                return i;
-            }
-        }
-
-        return -1;
-    }
 }
