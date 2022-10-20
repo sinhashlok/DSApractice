dc22f09b28f6532a88b427f5aa40dcc7bd96947e
sinhashlok <shlokjp@gmail.com>
2022-10-20 16:43:35 +0530

    update

--- Matrix/Medium/Valid_Sudoku_36.java
+++ Matrix/Medium/Valid_Sudoku_36.java
@@ -3,6 +3,18 @@ package DSApractice.Matrix.Medium;
 public class Valid_Sudoku_36 {
 
     public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9;) {
+            for (int j = 0; j < 9;) {
+                if (!check(board, i, j)) {
+                    return false;
+                }
+
+                j += 3;
+            }
+
+            i += 3;
+        }
+
         for (int i = 0, j = 0; i < 9 && j < 9; i++, j++) {
             boolean[] col = new boolean[10];
             for (int x = 0; x < 9; x++) {   // column
@@ -29,6 +41,26 @@ public class Valid_Sudoku_36 {
                     }
 
                     row[ch - '0'] = true;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean check(char[][] board, int a, int b) {
+        boolean[] arr = new boolean[10];
+
+        for(int i = a; i < a + 3; i++) {
+            for (int j = b; j < b + 3; j++) {
+                char ch = board[i][j];
+
+                if (ch != '.') {
+                    if (arr[ch - '0']) {
+                        return false;
+                    }
+
+                    arr[ch - '0'] = true;
                 }
             }
         }
