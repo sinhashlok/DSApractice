0f157514f44b491e87ce603780e1805016578fc3
sinhashlok <shlokjp@gmail.com>
2022-10-27 18:50:22 +0530

    update

---
+++ Strings/Easy/Check_if_One_String_Swap_Can_Make_Strings_Equal_1790.java
@@ -0,0 +1,17 @@
+package DSApractice.Strings.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Check_if_One_String_Swap_Can_Make_Strings_Equal_1790 {
+
+    public boolean areAlmostEqual(String s1, String s2) {
+        List<Integer> l = new ArrayList<>();
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) l.add(i);
+            if (l.size() > 2) return false;
+        }
+
+        return l.size() == 0 || (l.size() == 2 && s1.charAt(l.get(0)) == s2.charAt(l.get(1)) && s1.charAt(l.get(1)) == s2.charAt(l.get(0)));
+    }
+}
