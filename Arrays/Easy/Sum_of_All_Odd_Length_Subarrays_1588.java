dc947d01aa8816ffd6644e31c4a89aebca93cb7d
sinhashlok <shlokjp@gmail.com>
2022-10-29 14:32:37 +0530

    update

---
+++ Arrays/Easy/Sum_of_All_Odd_Length_Subarrays_1588.java
@@ -0,0 +1,14 @@
+package DSApractice.Arrays.Easy;
+
+public class Sum_of_All_Odd_Length_Subarrays_1588 {
+
+    public int sumOddLengthSubarrays(int[] arr) {
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += (((i + 1) * (arr.length - i) + 1) / 2) * arr[i];
+        }
+
+        return sum;
+    }
+}
