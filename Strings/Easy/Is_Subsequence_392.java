874c1058a57b721b0047704d3176b0ef8d68b3a4
sinhashlok <shlokjp@gmail.com>
2022-11-02 12:06:27 +0530

    update

---
+++ Strings/Easy/Is_Subsequence_392.java
@@ -0,0 +1,17 @@
+package DSApractice.Strings.Easy;
+
+public class Is_Subsequence_392 {
+
+    public boolean isSubsequence(String s, String t) {
+        int n = s.length(), m = t.length();
+        int j = 0;
+
+        for (int i = 0; i < m && j < n; i++) {
+            if (s.charAt(j) == t.charAt(i)) {
+                j++;
+            }
+        }
+
+        return (j == n);
+    }
+}
