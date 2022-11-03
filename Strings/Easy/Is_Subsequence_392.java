ac628251254083ac43e14112fe0ea688d3e1f68d
sinhashlok <shlokjp@gmail.com>
2022-11-03 18:42:07 +0530

    update

--- Strings/Easy/Is_Subsequence_392.java
+++ Strings/Easy/Is_Subsequence_392.java
@@ -3,7 +3,8 @@ package DSApractice.Strings.Easy;
 public class Is_Subsequence_392 {
 
     public boolean isSubsequence(String s, String t) {
-        int n = s.length(), m = t.length();
+        int n = s.length();
+        int m = t.length();
         int j = 0;
 
         for (int i = 0; i < m && j < n; i++) {
