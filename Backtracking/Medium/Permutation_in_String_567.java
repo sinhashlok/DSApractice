2a61004ba617f63eefe366d7ca598b6379cbdbd7
Shlok Sinha <shlokjp@gmail.com>
2023-03-05 23:58:00 +0530

    update

---
+++ Backtracking/Medium/Permutation_in_String_567.java
@@ -0,0 +1,41 @@
+package DSApractice.Backtracking.Medium;
+
+import java.util.Arrays;
+
+public class Permutation_in_String_567 {
+    // Naive Approach
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        char[] ch1 = s1.toCharArray();
+        Arrays.sort(ch1);
+
+        int l = 0, r = s1.length() - 1;
+        while (r < s2.length()) {
+            String s = s2.substring(l, r + 1);
+            char[] ch2 = s.toCharArray();
+            Arrays.sort(ch2);
+
+            if (helper(ch1, ch2)) {
+                return true;
+            }
+
+            l++;
+            r++;
+        }
+
+        return false;
+    }
+
+    private boolean helper(char[] ch1, char[] ch2) {
+        for (int i = 0; i < ch1.length; i++) {
+            if (ch1[i] != ch2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
