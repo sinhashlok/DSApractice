8b1447891ae9793cebfcb3ed4d2253155e21810c
Shlok Sinha <shlokjp@gmail.com>
2023-06-04 12:45:44 +0530

    update

--- Arrays/Easy/Two_Sum_1.java
+++ Arrays/Easy/Two_Sum_1.java
@@ -2,32 +2,24 @@
 
 package DSApractice.Arrays.Easy;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class Two_Sum_1 {
 
-
     public int[] twoSum(int[] nums, int target) {
-        HashSet<Integer> set = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            if (set.contains(target - nums[i]))
-                return new int[] {i, index(nums, target - nums[i], i)};
-            else
-                set.add(nums[i]);
+            if (map.containsKey(target - nums[i])) {
+                return new int[] {i, map.get(target - nums[i])};
+            }
+
+            map.put(nums[i], i);
         }
 
         return new int[] {-1, -1};
-    }
-
-    public int index(int[] nums, int x, int k) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (i == k)
-                continue;
-            if (x == nums[i])
-                return i;
-        }
-
-        return -1;
     }
 }
